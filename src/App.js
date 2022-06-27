0850087081ae5c54656febac84b09d3c85dbf76c
Benita Anawonah <benitaanawonah33@gmail.com>
2022-06-27 17:48:52 +0100

    ThemeSettings and ColorPicker

--- src/App.js
+++ src/App.js
@@ -44,6 +44,8 @@ const App = () => {
                 </div>
             
             <div>
+                <ThemeSettings />
+                
                 <Routes>
                     {/* {/* Dashboard  */}
                     <Route path='/' element={<Ecommerce />} />
